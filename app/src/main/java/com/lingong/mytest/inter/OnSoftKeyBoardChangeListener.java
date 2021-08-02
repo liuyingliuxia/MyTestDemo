54bfefd3c7be5d9de91ec3070509426d63b2e090
Miracle.lin <miracle.lin@zeasn.com>
2021-08-02 12:47:42 +0800

    新分支overlay demo_1

--- app/src/main/java/com/lingong/mytest/inter/OnSoftKeyBoardChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.lingong.mytest.inter;
-
-public interface OnSoftKeyBoardChangeListener {
-    void keyBoardShow();
-    void keyBoardHide();
-
-}
--- app/src/main/java/com/lingong/mytest/ui/base/BaseFragment.java
+++
@@ -1,9 +0,0 @@
-package com.lingong.mytest.ui.base;
-
-/**
- * Author:Miracle.lin
- * Date:2021/7/5 11:48
- * Descri:
- */
-public class BaseFragment {
-}
